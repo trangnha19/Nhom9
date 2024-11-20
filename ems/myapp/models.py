fa108d85d59704db20515ea4ae3dec9bf35c4bda
Nguyen Thi Trang Nha <129216994+trangnha19@users.noreply.github.com>
2024-11-20 12:28:36 +0700

    Update models.py

--- ems/myapp/models.py
+++ ems/myapp/models.py
@@ -1,3 +1,4 @@
+from datetime import time, timedelta
 from datetime import datetime, time
 from django.db import models
 from django.contrib.auth.models import User
@@ -28,20 +29,29 @@ class Position(models.Model):
 class Profile(models.Model):
     image = models.ImageField(upload_to='')
     cccd = models.CharField(max_length=12)
-    dob=models.DateField()
-    phone_number=models.CharField(max_length=12)
+    dob = models.DateField()
+    phone_number = models.CharField(max_length=12)
     start_date = models.DateField(auto_now_add=True)
     end_date = models.DateField(null=True, blank=True)
     address = models.CharField(max_length=200)
-    position=models.ForeignKey(Position,on_delete=models.CASCADE)
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
     gender = models.CharField(max_length=10, choices=[('Nam', 'Nam'), ('Nữ', 'Nữ')])
     degree = models.CharField(max_length=20, choices=[('Đại học', 'Đại học'), ('Cao đẳng', 'Cao đẳng')])
     major = models.CharField(max_length=100)
     contract_period = models.CharField(max_length=10)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
+    salary = models.IntegerField(default=0)
+    # Thêm cột trạng thái
+    STATUS_CHOICES = [
+        ('working', 'Đang làm'),
+        ('on_leave', 'Nghỉ phép'),
+        ('resigned', 'Đã nghỉ việc'),
+    ]
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='working')
 
     def __str__(self):
          return f'Profile: {self.user.username}'
+
    
 
 class TopicLetter(models.Model):
@@ -74,26 +84,25 @@ class Sheet(models.Model):
     checkout=models.TimeField(blank=True, null=True)
     work_hour=models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
     salary=models.DecimalField(max_digits=10, decimal_places=0, blank=True, null=True)
-    status=models.CharField(max_length=20, choices=[('Đúng Giờ', 'Đúng Giờ'), ('Muộn', 'Muộn')], default='Đúng Giờ')
+    status=models.CharField(max_length=20, choices=[('Đúng Giờ', 'Đúng Giờ'), ('Đến Muộn', 'Đến Muộn'), ('Về Sớm', 'Về Sớm')], default='Đúng Giờ')
+    ot = models.IntegerField(null=True, blank=True, default=1)
 
     def __str__(self):
         return f'{self.user.username} - {self.date}'
 
-    def is_late(self):
-        if self.checkin > time(8, 0, 0):
-            self.status = 'Muộn'
 
-    def calculate_salary(self):
-        profile = Profile.objects.get(user=self.user)
-        if self.work_hour is not None:
-            if self.status == 'Muộn':
-                salary_1hour = profile.position.department.salary * Decimal(0.8)
-            else:
-                salary_1hour = profile.position.department.salary
-            rate = profile.position.salary_coef * salary_1hour
-            self.salary = rate * Decimal(self.work_hour)
+    def update_status(self):
+        # Check if the user is late (after 8 AM) or leaves early (before 6 PM)
+        if self.checkin > time(8, 0, 0):
+            self.status = 'Đến Muộn'
+        elif self.checkout and self.checkout < time(17, 0, 0):
+            self.status = 'Về Sớm'
         else:
-            self.salary = 0
+            self.status = 'Đúng Giờ'
+        
+        # Calculate overtime (OT) if checkout time is after 6 PM
+        if self.work_hour:
+            self.ot = max(0, int(self.work_hour - 9))
 
     def save(self, *args, **kwargs):
         if self.checkin and self.checkout:
@@ -101,6 +110,5 @@ class Sheet(models.Model):
             checkout = timezone.datetime.combine(self.date, self.checkout)
             self.work_hour = (checkout - checkin).seconds/3600
 
-        self.is_late()
-        self.calculate_salary()
+        self.update_status()
         super().save( *args, **kwargs)
